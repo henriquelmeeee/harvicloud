9c909fb9f97bc3613d18e366fbfc2008ed2a7e8a
Henrique <86432400+henriquelmeeee@users.noreply.github.com>
2022-06-06 11:55:27 -0300

    Update API
    Finish hosting websites with docker containers and start hosting bots

--- v1/src/api.harvicloud.com/server.py
+++ v1/src/api.harvicloud.com/server.py
@@ -5,21 +5,19 @@ abril de 2022
 ---------------------------------------------
 """
 
-from flask import Flask, render_template, url_for, redirect, request, session
+from flask import Flask, render_template, url_for, redirect, request, session, jsonify
 from flaskext.mysql import MySQL
 import os, random
 from datetime import datetime
-
 import subprocess
 
 template_dir = os.path.abspath('.')
 static_dir = os.path.abspath('.')
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
-app.config['SECRET_KEY'] = "secret_key_here"
+app.config['SECRET_KEY'] = "YAS5453278FDSHDWE456"
 
 import mysql.connector
 from mysql.connector import Error
-cursor = None
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'user'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
@@ -33,11 +31,6 @@ cursor = conn.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS users (user text, password text, token text, plano text);")
 cursor.execute("CREATE TABLE IF NOT EXISTS gitea (user text, id text, porta text, name text, online text)")
 cursor.execute("CREATE TABLE IF NOT EXISTS nextcloud (user text, id text, porta text, name text, online text)")
-
-def turn_on_containers():
-    pass
-    # Em produção...
-turn_on_containers()
 
 def is_alphanum(text):
     is_alphanum = True
@@ -54,6 +47,16 @@ def is_alphanum(text):
     else:
         return False
         
+
+def generate_nonce(caracteres):
+    nonce = ''
+    for s in range (0, caracteres):
+        valor = random.choice(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+        if random.randint(1, 5) == 1:
+            valor = valor.upper()
+        nonce += valor
+    return nonce
+
 @app.route('/')
 def home():
     return redirect(url_for('explore'))
@@ -64,6 +67,7 @@ def explore():
 
 @app.route('/v1/auth/login/', methods=["POST"])
 def autenticar():
+    conn = mysql.connect(); cursor = conn.cursor()
     if not "user" in session:
         content = request.json; user=str(content["user"]); password=str(content["password"])
         if is_alphanum(user) and is_alphanum(password):
@@ -81,7 +85,7 @@ def autenticar():
 
 
 def discord_send_log(mensagem):
-    # tem que checar se existe a pasta do ano, do mes e o txt do dia
+    # ...
     today = datetime.now()
     file = open(f'./v1/logs/{today.year}/{today.month}/discord/{str(today.day)}.txt', "a")
     file.write(f'{mensagem}')
@@ -102,7 +106,7 @@ def send_log():
                 )
             return ''
         else:
-            return 'Não autorizado', 401
+            return jsonify({"error": "not authorized"}), 401
 
 
 os.system('rm ./v1/discord/bot/logs/logs.txt'); os.system('touch ./v1/discord/bot/logs/logs.txt')
@@ -111,18 +115,18 @@ def logs():
     if request.method == 'POST':
         content = request.json
         if not str(content["secret"]) == 'ASKOGSDITR574569JFG6':
-            return 'Não autorizado', 401
+            return jsonify({"error": "not authorized"}), 401
         else:
             logs = str(content["logs"])
             f = open('./v1/discord/bot/logs/logs.txt', "a")
             f.write(logs + '\n')
             f.close()
-            return 'Logs enviadas com sucesso', 200
+            return jsonify({"status": "logs sent successfully"}), 200
     else:
         if "user" in session and not str(session["user"]) == 'henrique': 
-            return 'Não autorizado', 403
+            return jsonify({"error": "not authorized"}), 403
         elif not "user" in session:
-            return 'Não autorizado', 401
+            return jsonify({"error": "not authorized"}), 401
         else:
             return render_template('./v1/discord/bot/logs/index.html', log="a")
 
@@ -151,89 +155,122 @@ def validar_token():
     if request.method == "POST":
         content = request.json
         if not str(content["secret"]) == 'ASKOGSDITR574569JFG6':
-            return 'Não autorizado', 401
+            return jsonify({"error": "not authorized"}), 401
         else:
             token = str(content["token"])
             cursor.execute("SELECT token FROM users WHERE token='"+str(token)+"';")
             checar = cursor.fetchall()
             if str(checar) == '()':
-                return 'Token incorreto', 401
+                return jsonify({"error": "invalid token"}), 401
             else:
-                return 'Autorizado', 200
+                return jsonify({"status": "request successfully accepted"}), 200
 
-@app.route('/v1/web/gitea/views')
-def views_gitea():
-    try:
-        id_view = int(request.args.get('id'))
-    except:
-        return 'ID inválido', 400
-    else:
-        print(id_view)
-        # continuar
+###
 
-@app.route('/v1/web/create/<id_site>', methods=["GET"])
-def create_website(id_site):
-    id_site = str(id_site)
+websites = ['gitea', 'nextcloud', 'wordpress']
+
+@app.route('/v1/web/create/', methods=["POST"])
+def create_website():
+    content = request.json
+    conn = mysql.connect(); cursor = conn.cursor()
+    session["user"] = 'a'
+    id_site = content["id"]
     if id_site == 'nextcloud' or id_site == 'gitea':
-        website_name = request.args.get('website_name')
+        website_name = content['website_name']
         if website_name is None or len(website_name) > 18 or website_name == '':
-            return 'Nome do site inválido'
+            return jsonify({"error": "invalid website name"})
         else:
             if is_alphanum(str(website_name)):
-                if "user" in session:
-                    id_container = 0; porta = 0
-                    while True:
-                        ids = ['gitea', 'nextcloud']
-                        porta = random.randint(81, 60000)
-                        n = 0
-                        gitea_sites = []; nextcloud_sites = []
-                        for name in ids:
-                            n += 1
-                            if n == 1500:
-                                return 'Um erro ocorreu, tente novamente', 500
-                            else:
-                                cursor.execute(f"SELECT porta FROM {name} WHERE porta='{porta}' AND user='"+str(session["user"])+"';")
-                                valor = cursor.fetchall()
-                                if name == 'gitea':
-                                    gitea_sites = valor
-                                elif name == 'nextcloud':
-                                    nextcloud_sites = valor
-                                if str(valor) == '()' and n == len(ids):
-                                    break
-                                else:
-                                    continue
-                        cursor.execute(f"SELECT plano FROM users WHERE user='{str(session['user'])}'")
-                        plano = cursor.fetchall()
-                        total = len(gitea_sites) + len(nextcloud_sites)
-                        if 'free' in plano and total > 0 or 'profissional' in plano and total > 1 or 'empreendedor' in plano and total > 4:
-                            return 'Limite de websites atingido', 403
+                valor = '()'
+                for s in websites:
+                    cursor.execute(f"SELECT name FROM {s} WHERE name='{str(website_name)}'")
+                    valor = str(cursor.fetchall())
+                    if not str(valor) == '()':
                         break
-                    while True:
-                        id_container = random.randint(10, 100000)
-                        cursor.execute(f"SELECT id FROM {id_site} WHERE id='{id_container}' AND user='"+str(session["user"])+"';")
-                        valor = cursor.fetchall()
-                        if str(valor) == '()' and id_container != 443:
+                if valor == '()':
+                    if "user" in session:
+                        id_container = 0; porta = 0
+                        while True:
+                            ids = ['gitea', 'nextcloud']
+                            porta = random.randint(81, 60000)
+                            n = 0
+                            gitea_sites = []; nextcloud_sites = []
+                            for name in ids:
+                                n += 1
+                                if n == 1500:
+                                    return jsonify({"error": "the back end did not find a valid port for your application after several attempts, please try again"}), 500
+                                else:
+                                    cursor.execute(f"SELECT porta FROM {name} WHERE porta='{porta}' AND user='"+str(session["user"])+"';")
+                                    valor = cursor.fetchall()
+                                    if name == 'gitea':
+                                        gitea_sites = valor
+                                    elif name == 'nextcloud':
+                                        nextcloud_sites = valor
+                                    if str(valor) == '()' and n == len(ids):
+                                        break
+                                    else:
+                                        continue
+                            cursor.execute(f"SELECT plano FROM users WHERE user='{str(session['user'])}'")
+                            plano = cursor.fetchall()
+                            total = len(gitea_sites) + len(nextcloud_sites)
+                            if 'free' in plano and total > 0 or 'profissional' in plano and total > 1 or 'empreendedor' in plano and total > 4:
+                                return jsonify({"error": "website limit reached"}), 403
                             break
-                        else:
-                            continue
-                    valor = '-'
-                    for n in range(0, 7):
-                        valor += random.choice(["_", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
-                    id_container = str(id_container)
-                    os.system(f'mkdir ./v1/containers/web/data/{id_site}/{id_container + valor}')
-                    if id_site == 'nextcloud':
-                        os.system(f'docker container run --name {id_container + valor} -v {os.getcwd()}/v1/containers/web/data/nextcloud/{id_container + valor}:/var/www/html --memory="110m" -d -p {porta}:80 nextcloud')
-                    elif id_site == 'gitea':
-                        os.system(f'docker container run --name {id_container + valor} -v {os.getcwd()}/v1/containers/web/data/gitea/{id_container + valor}:/data --memory="160m" -d -p {porta}:3000 gitea/gitea')
-                    id_container = subprocess.check_output(f"docker ps -aqf \"name={id_container + valor}\"", shell=True)
-                    id_container = str(id_container).replace('b', '').replace('\\n', '').replace('\'', '')
-                    return id_container
-                  # Em produção...
+                        while True:
+                            id_container = random.randint(10, 100000)
+                            cursor.execute(f"SELECT id FROM {id_site} WHERE id='{id_container}' AND user='"+str(session["user"])+"';")
+                            valor = cursor.fetchall()
+                            if str(valor) == '()' and id_container != 443 and id_container != 3306:
+                                break
+                            else:
+                                continue
+                        valor = '-'
+                        for n in range(0, 7):
+                            valor += random.choice(["_", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+                        id_container = str(id_container)
+                        os.system(f'mkdir ./v1/containers/web/data/{id_site}/{id_container + valor}')
+                        if id_site == 'nextcloud':
+                            os.system(f'docker container run --name {id_container + valor} -v {os.getcwd()}/v1/containers/web/data/nextcloud/{id_container + valor}:/var/www/html --memory="110m" --memory-reservation="100m" --memory-swap="200m" --cpus="1" --restart=on-failure:10 --security-opt no-new-privileges -d -p {porta}:80 nextcloud')
+                        elif id_site == 'gitea':
+                            os.system(f'docker container run --name {id_container + valor} -v {os.getcwd()}/v1/containers/web/data/gitea/{id_container + valor}:/data --memory="160m" --memory-reservation="150m" --memory-swap="200m" --cpus="1" --restart=on-failure:10 --security-opt no-new-privileges -d -p {porta}:3000 gitea/gitea')
+                        id_container = subprocess.check_output(f"docker ps -aqf \"name={id_container + valor}\"", shell=True)
+                        id_container = str(id_container).replace('b', '').replace('\\n', '').replace('\'', '')
+                        cursor.execute(f"INSERT INTO {id_site} VALUES ('{str(session['user'])}', '{id_container}', '{porta}', '{website_name}', 'true')")
+                        conn.commit()
+                        return jsonify({"status": "request accepted successfully"})
+                    else:
+                        return redirect('http://harvicloud.com/login/')
                 else:
-                    return redirect('http://harvicloud.com/login/')
+                    return jsonify({"error": "there is already a website with this name"}), 406
             else:
-                return 'O nome do site deve ser apenas alfanumérico'
+                return 'Parâmetro inválido', 406
     else:
-        return redirect('http://harvicloud.com/')
+        return jsonify({"error": "invalid website type"})
 
-app.run(host='0.0.0.0', port=80, debug=False)
+###
+
+@app.route('/v1/bot/create/', methods=["POST"])
+def create_bot():
+    content = request.json
+    conn = mysql.connect(); cursor = conn.cursor()
+
+def validate_file(filename):
+    filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/v1/bot/upload-files/', methods=["POST"])
+def bot_upload_files():
+    if 'file' not in request.files:
+        return jsonify({"error": "no files were sent"})
+    else:
+        f = request.files["file"]
+        if f.filename == '':
+            return jsonify({"error": "invalid file name"})
+        else:
+            if f and allowed_file(f.filename):
+                filename = secure_filename(f.filename)
+                # ...
+                f.save(os.path.join(f'./v1/containers/discord/bot/data/{directory}/', name=filename))
+                return jsonify({"status": "file saved successfully"})
+
+
+app.run(host='0.0.0.0', port=80, debug=True)
